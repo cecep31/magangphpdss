650973cca8ad50bd09cc373f59afaee9c560a73f
cecep31 <cecepjanuardi31@gmail.com>
2020-10-13 12:00:05 +0700

    normalisasi

--- init.php
+++ init.php
@@ -1,1 +1,13 @@
-kjdkjakjsdkjaskdjakdadkjadjkasjsjsjsjdhdnhjfknhdsnsnsnsnnsnns
+
+<?php
+include "config\app.php";
+$sql = mysqli_query(Datab(),"SELECT * FROM `altnilai` LIMIT 1");
+$asd = mysqli_query(Datab(),"SELECT count(*) AS NUMBEROFCOLUMNS FROM information_schema.columns WHERE table_name = 'altnilai'");
+while ($rasd = mysqli_fetch_array($asd)) {
+    $jmlfl =  $rasd[0];
+}
+$ria= mysqli_query(Datab(),"desc altnilai");
+while ($ty=mysqli_fetch_array($ria)) {
+    echo $ty["0"];
+    echo "<br>";
+}
--- ranking.php
+++ ranking.php
@@ -1,7 +1,42 @@
+<div class="container">
 <?php
 
-$go = mysqli_query(Datab(), "select * from altnormal");
-
-
+$golo = mysqli_query(Datab(), "select * from altnilai order by ida asc");
+$ni=0;
+$cars = array();
+$ria= mysqli_query(Datab(),"desc altnilai");
+while ($ty=mysqli_fetch_array($ria)) {
+    echo $ty["0"];
+    echo "<br>";
+}
+$nok=1;
+$nok2=2;
+$nokkat=1;
+$kriteria=mysqli_query(Datab(),"select * from kriteria order by idk asc");
+while ($tar=mysqli_fetch_array($kriteria)) {
+    if ($tar["3"]=="b") {
+        $go=mysqli_query(Datab(),"select '$tar[$nok]' from altnilai order by ida asc");
+        $lai = mysqli_query(Datab(), "SELECT max($tar[$nok]) AS max FROM altnilai");
+        while ($rt=mysqli_fetch_array($go)) {
+            $rt[$nok];
+        }
+        echo "bangsat";
+    }else {
+        $go=mysqli_query(Datab(),"select * from altnilai order by ida asc limit $nok,$nok2");
+        $lai = mysqli_query(Datab(), "SELECT min($tar[$nok]) AS max FROM altnilai");
+        while ($rt=mysqli_fetch_array($go)) {
+            $rt[$nok];
+        }
+        echo "cok";
+    }
+    $nok += 1;
+}
+$no=1;
+// while ($yu=mysqli_fetch_array($go)) {
+//     $lai = mysqli_query(Datab(), "SELECT max($yu[1]) AS max FROM altnilai");
+//     $sqlki=mysqli_query(Datab(),"select '$yu[1]' from kriteria where idk  ");
+    
+// }
 
 ?>
+</div>
