3efd0bf097335a82300d70d34225b0f777cfb2d6
cecep januardi <cecepjanuardi31@gmail.com>
2020-10-20 11:10:50 +0700

    api work

--- api/rank.php
+++ api/rank.php
@@ -1,7 +1,32 @@
 <?php
-include "./config/app.php";
-$conn = new mysqli("localhost","pilput","pilput31", "magangdss");
-$gou=mysqli_query($conn,"SELECT alternatif.ida,namaa,nilai FROM `ranknya`,alternatif WHERE ranknya.ida=alternatif.ida ORDER BY nilai DESC limit 10"); 
-                
-echo json_encode($gou);
+function Datab()
+{
+    # code...
+    $host = "localhost";
+    $user = "pilput";
+    $pass = "pilput31";
+    $db = "magangdss";
+
+        $conn = new mysqli($host,$user,$pass, $db);
+        if ($conn->connect_error) {
+            die("connection failed: ". $conn->connect_error);
+        }
+        // else{echo 'berhasil connect';}
+
+        return $conn;
+}
+$no=1;
+$json=[];
+$sql = "SELECT ranknya.ida, alternatif.namaa, ranknya.nilai FROM ranknya,alternatif WHERE alternatif.ida = ranknya.ida ORDER BY nilai DESC";
+$gou=mysqli_query(Datab(),$sql); 
+while ($sd= mysqli_fetch_array($gou)) {
+    $json[$no] = [
+        'id' => $sd["0"],
+        'nama' => $sd["1"],
+        'nilai' => $sd["2"],
+        ];
+        $no += 1;
+    }
+$data = ['ranking' => $json];
+echo json_encode($data);
 echo "json";
--- home.php
+++ home.php
@@ -1,6 +1,18 @@
 <br>
 <div class="container">
     <div class="row">
-        <h1>berada di home</h1>
+        <h2>Selamat datang di system pendukung keputusan metode SAW</h2>
+    </div>
+    <br>
+    <div class="row">
+        <p>
+            petunjuk pemakaian: <br> <br>
+            -> user harus melakukan penginputan data kriteria <br>
+            -> user harus menginputkan data bobot kriteria menggunakan bilangan berkoma atau pecahan <br>
+            -> total bobot tidak boleh lebih atau kurang dari 1 <br>
+            -> dalam pengisian atau memberikan nilai pada alternatif secara default nilainya akan 0 maka silahkan di isi <br>
+            -> sebelum melakukan output diharuskan memberikan terlebih dahulu agar ke akuratan perhitungan <br>
+            -> pada bagian get data hanya link untuk api hasil perengkingan saja
+        </p>
     </div>
 </div>
